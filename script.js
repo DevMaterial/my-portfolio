83249d73cf2a7669ce165506e76f2f71ab79c797
Travis <developer.material@gmail.com>
2016-05-15 11:58:03 -0400

    updated with new project

--- script.js
+++ script.js
@@ -11,3 +11,22 @@ $(document).ready(function(){
 
   });
 });
+
+$(document).ready(function(){
+
+    setBindings();
+
+});
+
+function setBindings() {
+
+  $("nav a").click(function(e){
+    e.preventDefault();
+      var sectionID = e.currentTarget.id + "Section";
+
+      $("html body").animate({
+        scrollTop: $("#" + sectionID).offset().top / 1.1
+      }, 1000);
+
+  });
+}
